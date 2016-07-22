abf1e096ce4b86e14a50342dd95ac24a3f89ac1e
Cyril Plisko <cyril.plisko@mountall.com>
2016-07-22 18:37:01 +0300

    Another benchmark

--- src/humantime.rs
+++ src/humantime.rs
@@ -260,4 +260,13 @@ mod tests {
         let ht = HumanTime::from(Duration::weeks(2));
         b.iter(|| format!("{}", ht));
     }
+
+    #[bench]
+    fn long_string(b: &mut Bencher) {
+        let ht = HumanTime::from(Duration::seconds(1234567890));
+        b.iter(|| format!("{:#}", ht));
+        // let english = format!("{:#}", ht);
+        // assert_eq!("in 39 years, 1 month, 3 weeks, 2 days, 23 hours, 31 minutes and 30 seconds",
+        //            english);
+    }
 }
