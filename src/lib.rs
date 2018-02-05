f0f7d93ff1751ffc2437b2913308724b4b75d7a9
Cyril Plisko <cyril.plisko@mountall.com>
2018-02-06 01:03:40 +0200

    Add doctest for HumanTime::to_text_en()

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,23 @@
 //! assert_eq!("an hour ago", format!("{}", ht));
 //! ```
 //!
+//! For full control over the text representation use `HumanTime::to_text_en()`
+//!
+//! ```
+//! extern crate chrono;
+//! extern crate chrono_humanize;
+//!
+//! use chrono::Duration;
+//! use chrono_humanize::{Accuracy, HumanTime, Tense};
+//!
+//! # fn main() {
+//! let dt = Duration::days(45);
+//! let ht = HumanTime::from(dt);
+//!
+//! assert_eq!("a month", ht.to_text_en(Accuracy::Rough, Tense::Present));
+//! assert_eq!("1 month, 2 weeks and 1 day", ht.to_text_en(Accuracy::Precise, Tense::Present));
+//! # }
+//! ```
 
 #![cfg_attr(all(feature = "cargo-clippy", feature = "pedantic"), warn(clippy_pedantic))]
 #![cfg_attr(feature = "cargo-clippy", warn(use_self))]
