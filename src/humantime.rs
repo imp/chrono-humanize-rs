c15c4a9958b13f3b4188c6e509f1799c9d5e3bd4
Cyril Plisko <cyril.plisko@mountall.com>
2018-02-05 20:45:16 +0200

    Massive refactor

--- src/humantime.rs
+++ src/humantime.rs
@@ -1,8 +1,57 @@
-use std::fmt;
 use std::cmp::max;
-use std::convert::From;
+use std::fmt;
+
 use chrono::{DateTime, Duration, TimeZone, Utc};
-use super::Humanize;
+
+use Humanize;
+
+
+/// Indicates the time of the period in relation to the time of the utterance
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum Tense {
+    Past,
+    Present,
+    Future,
+}
+
+/// The accuracy of the representation
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum Accuracy {
+    /// Rough approximation, easy to grasp, but not necessarily accurate
+    Rough,
+    /// Concise expression, accurate, but not necessarily easy to grasp
+    Precise,
+}
+
+impl Accuracy {
+    /// Returns whether this accuracy is precise
+    pub fn is_precise(&self) -> bool {
+        *self == Accuracy::Precise
+    }
+
+    /// Returns whether this accuracy is rough
+    pub fn is_rough(&self) -> bool {
+        *self == Accuracy::Rough
+    }
+}
+
+impl From<bool> for Accuracy {
+    fn from(precise: bool) -> Self {
+        if precise {
+            Accuracy::Precise
+        } else {
+            Accuracy::Rough
+        }
+    }
+}
+
+// Number of seconds in various time periods
+const MINUTE: i64 = 60;
+const HOUR: i64 = MINUTE * 60;
+const DAY: i64 = HOUR * 24;
+const WEEK: i64 = DAY * 7;
+const MONTH: i64 = DAY * 30;
+const YEAR: i64 = DAY * 365;
 
 #[derive(Debug)]
 enum TimePeriod {
@@ -60,60 +109,51 @@ impl TimePeriod {
         }
     }
 
-    pub fn to_string(&self, precise: bool) -> String {
-        if precise {
-            self.to_string_precise()
-        } else {
-            self.to_string_rough()
+    fn to_string(&self, accuracy: Accuracy) -> String {
+        match accuracy {
+            Accuracy::Rough => self.to_string_rough(),
+            Accuracy::Precise => self.to_string_precise(),
         }
     }
 }
 
-#[derive(Debug)]
-enum Tense {
-    Past,
-    Present,
-    Future,
-}
-
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
 pub struct HumanTime(Duration);
 
 impl HumanTime {
-    fn humanize(&self, precise: bool) -> (Vec<TimePeriod>, Tense) {
+    fn humanize(&self, accuracy: Accuracy) -> (Vec<TimePeriod>, Tense) {
         use std::i64::{MIN, MAX};
 
         let tense = match self.0.num_seconds() {
-            -10...10 if !precise => Tense::Present,
+            -10...10 if accuracy.is_rough() => Tense::Present,
             MIN...-1 => Tense::Past,
             1...MAX => Tense::Future,
             _ => Tense::Present,
         };
 
-        let periods = if precise {
-            HumanTime::precise_period(self.0)
+        let periods = if accuracy.is_precise() {
+            Self::precise_period(self.0)
         } else {
-            vec![HumanTime::rough_period(self.0)]
+            vec![Self::rough_period(self.0)]
         };
         (periods, tense)
     }
 
     fn rough_period(duration: Duration) -> TimePeriod {
         use self::TimePeriod::*;
-        // use std::i64::MAX;
 
         match duration.num_seconds().abs() {
-            n if n > 47260800 => Years(max(n / 31536000, 2)),
-            n if n > 29808000 => Years(1),
-            n if n > 3888000 => Months(max(n / 2592000, 2)),
-            n if n > 2505600 => Months(1),
-            n if n > 907200 => Weeks(max(n / 604800, 2)),
-            n if n > 561600 => Weeks(1),
-            n if n > 129600 => Days(max(n / 86400, 2)),
-            n if n > 79200 => Days(1),
-            n if n > 5400 => Hours(max(n / 3600, 2)),
-            n if n > 2700 => Hours(1),
-            n if n > 90 => Minutes(max(n / 60, 2)),
+            n if n > 547 * DAY => Years(max(n / YEAR, 2)),
+            n if n > 345 * DAY => Years(1),
+            n if n > 45 * DAY => Months(max(n / MONTH, 2)),
+            n if n > 29 * DAY => Months(1),
+            n if n > 10 * DAY + 12 * HOUR => Weeks(max(n / WEEK, 2)),
+            n if n > 6 * DAY + 12 * HOUR => Weeks(1),
+            n if n > 36 * HOUR => Days(max(n / DAY, 2)),
+            n if n > 22 * HOUR => Days(1),
+            n if n > 90 * MINUTE => Hours(max(n / HOUR, 2)),
+            n if n > 45 * MINUTE => Hours(1),
+            n if n > 90 => Minutes(max(n / MINUTE, 2)),
             n if n > 45 => Minutes(1),
             n if n > 10 => Seconds(n),
             0...10 => Now,
@@ -142,45 +182,39 @@ impl HumanTime {
     fn precise_period(duration: Duration) -> Vec<TimePeriod> {
         use self::TimePeriod::*;
 
-        let year = Duration::days(365).num_seconds();
-        let month = Duration::days(30).num_seconds();
-        let week = Duration::weeks(1).num_seconds();
-        let day = Duration::days(1).num_seconds();
-        let hour = Duration::hours(1).num_seconds();
-        let minute = Duration::minutes(1).num_seconds();
         let zero = Duration::zero().num_seconds();
 
         let mut duration = duration.num_seconds().abs();
         let mut periods = Vec::<TimePeriod>::new();
 
-        if duration >= year {
-            periods.push(Years(duration / year));
-            duration %= year;
+        if duration >= YEAR {
+            periods.push(Years(duration / YEAR));
+            duration %= YEAR;
         }
 
-        if duration >= month {
-            periods.push(Months(duration / month));
-            duration %= month;
+        if duration >= MONTH {
+            periods.push(Months(duration / MONTH));
+            duration %= MONTH;
         }
 
-        if duration >= week {
-            periods.push(Weeks(duration / week));
-            duration %= week;
+        if duration >= WEEK {
+            periods.push(Weeks(duration / WEEK));
+            duration %= WEEK;
         }
 
-        if duration >= day {
-            periods.push(Days(duration / day));
-            duration %= day;
+        if duration >= DAY {
+            periods.push(Days(duration / DAY));
+            duration %= DAY;
         }
 
-        if duration >= hour {
-            periods.push(Hours(duration / hour));
-            duration %= hour;
+        if duration >= HOUR {
+            periods.push(Hours(duration / HOUR));
+            duration %= HOUR;
         }
 
-        if duration >= minute {
-            periods.push(Minutes(duration / minute));
-            duration %= minute;
+        if duration >= MINUTE {
+            periods.push(Minutes(duration / MINUTE));
+            duration %= MINUTE;
         }
 
         if duration > zero || periods.is_empty() {
@@ -190,30 +224,34 @@ impl HumanTime {
         periods
     }
 
-    fn locale_en(&self, precise: bool) -> String {
-        let (mut periods, tense) = self.humanize(precise);
-        let mut time = periods.remove(0).to_string(precise);
-        let last = periods.pop().map(|p| p.to_string(precise));
+    fn locale_en(&self, accuracy: Accuracy) -> String {
+        let (mut periods, tense) = self.humanize(accuracy);
+        let mut time = periods.remove(0).to_string(accuracy);
+        let last = periods.pop().map(|p| p.to_string(accuracy));
         for period in periods {
-            time = time + ", " + &period.to_string(precise);
+            time = time + ", " + &period.to_string(accuracy);
         }
 
         if let Some(last) = last {
             time = time + " and " + &last;
         }
 
+        Self::tense_en(tense, &time)
+    }
+
+    pub fn tense_en(tense: Tense, text: &str) -> String {
         match tense {
-            Tense::Past => format!("{} ago", time),
-            Tense::Future => format!("in {}", time),
-            Tense::Present => time,
+            Tense::Past => format!("{} ago", text),
+            Tense::Future => format!("in {}", text),
+            Tense::Present => format!("{}", text),
         }
     }
 }
 
 impl fmt::Display for HumanTime {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let precise = f.alternate();
-        f.pad(&self.locale_en(precise))
+        let accuracy = f.alternate().into();
+        f.pad(&self.locale_en(accuracy))
     }
 }
 
@@ -228,7 +266,7 @@ where
     TZ: TimeZone,
 {
     fn from(dt: DateTime<TZ>) -> Self {
-        HumanTime::from(dt.signed_duration_since(Utc::now()))
+        Self::from(dt.signed_duration_since(Utc::now()))
     }
 }
 
